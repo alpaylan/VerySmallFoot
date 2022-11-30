483b41dea5556d9a70a9f41288abe203d08d701f
alpaylan <alpkeles99@gmail.com>
2022-11-30 12:12:33 -0500

    add heap_race, make tests into HUnit tests

--- smallfootreplica/test/Spec.hs
+++ smallfootreplica/test/Spec.hs
@@ -2,7 +2,7 @@
 import Program
 import VariableConditions
 import qualified Data.Set as Set
-
+import Test.HUnit
 
 
 --  tree_copy(s;t) [tree(t)] {
@@ -20,8 +20,8 @@ import qualified Data.Set as Set
 -- } [tree(s) * tree(t)]
 
 -- name, pass-by-ref args, pass-by-value args, local vars, body
-exampleProgram :: Program
-exampleProgram =
+treeCopy :: Program
+treeCopy =
   Program
     ["l", "r"]
     []
@@ -42,8 +42,6 @@ exampleProgram =
           ])
         , AssertConj [] [HeapTree (Var "s"), HeapTree (Var "t")])]
 
-
-
 comm :: Command
 comm =  Block
         [ HeapLookup "i" (Var "t") "l"
@@ -55,23 +53,74 @@ comm =  Block
         , HeapAssign (Var "s") "r" (Var "jj")
         ]
 
-
-
-checkVarC :: Bool
+checkVarC :: Test
 checkVarC =
-    varC (mkContext exampleProgram) comm == Set.fromList ["i", "ii", "j", "jj", "s", "t"]
+    TestCase $ assertEqual 
+                "All variables in the command should be in the set" 
+                (varC (mkContext treeCopy) comm) 
+                (Set.fromList ["i", "ii", "j", "jj", "s", "t"])
 
-
-checkModC :: Bool
+checkModC :: Test
 checkModC =
-    modC (mkContext exampleProgram) comm == Set.fromList ["i", "ii", "j", "jj", "s"]
-
-        
-checkReqC :: Bool
+    TestCase $ assertEqual
+    "All variables modified in the command should be in the set"
+    (modC (mkContext treeCopy) comm)
+    (Set.fromList ["i", "ii", "j", "jj", "s"])
+ 
+checkReqC :: Test
 checkReqC =
-    reqC (mkContext exampleProgram) comm == Set.fromList []
+    TestCase $ assertEqual
+    "All resources required in the command should be in the set"
+    (reqC (mkContext treeCopy) comm)
+    Set.empty
+
+checkParTreeCopy :: Test
+checkParTreeCopy =
+    TestCase $ assertEqual
+    "All functions called in parallel with tree_copy must be in the set"
+    (par (mkContext treeCopy) "tree_copy")
+    Set.empty
 
 
+-- tl;
+
+-- update(x,y) [x|->] {
+--   x->tl = y;
+-- } [x|->y]
+
+-- heap_race() {
+--   c = new();
+--   update(c,42) || update(c,13);
+-- } [c|->]
+    
+heapRace :: Program
+heapRace = 
+    Program
+    ["tl"]
+    []
+    [Function
+       "update" ["x"] ["y"]
+       []
+       (AssertConj [] [PointsTo (Var "x") []]
+       ,HeapAssign (Var "x") "tl" (Var "y")
+       ,AssertConj [] [PointsTo (Var "x") [("tl", Var "y")]])
+    , Function
+        "heap_race" [] []
+        []
+        (AssertConj [] [],
+        Block
+            [ New "c"
+            , ConcurrentCall ("update", ["c"], [Const 42]) ("update", ["c"], [Const 13])
+            ]
+        , AssertConj [] [PointsTo (Var "c") []])]
+    
+
+checkParHeapRace :: Test
+checkParHeapRace =
+    TestCase $ assertEqual
+    "All functions called in parallel with update must be in the set"
+    (par (mkContext heapRace) "update") 
+    (Set.fromList ["update"])
 
 main :: IO ()
-main = print checkVarC >> print checkModC >> print checkReqC
+main = runTestTT (TestList [checkVarC, checkModC, checkReqC, checkParTreeCopy, checkParHeapRace]) >> return ()
