cabb2f80cba297109f486967e1a4283ecbdb79bd
alpaylan <alpkeles99@gmail.com>
2022-11-30 09:51:38 -0500

    change mod, var, req for accomodating least fixpoint computations

--- smallfootreplica/src/VariableConditions.hs
+++ smallfootreplica/src/VariableConditions.hs
@@ -98,6 +98,64 @@ getResource (gamma, _) rname =
   fromMaybe (error "Resource not found") $ Map.lookup rname gamma
 
 
+getCalls :: Context -> FunName -> Set FunName
+getCalls ctx fname = getCallsF (getFunction ctx fname)
+  where
+    getCallsF :: Function -> Set FunName
+    getCallsF (Function _ _ _ _ (_, c, _)) = getCallsC c
+
+    getCallsC :: Command -> Set FunName
+    getCallsC (Assign _ _) = Set.empty
+    getCallsC (HeapLookup {}) = Set.empty
+    getCallsC (HeapAssign {}) = Set.empty
+    getCallsC (New _) = Set.empty
+    getCallsC (Dispose _) = Set.empty
+    getCallsC (IfThenElse _ c1 c2) = getCallsC c1 \/ getCallsC c2
+    getCallsC (While _ _ c) = getCallsC c
+    getCallsC (Call (f, _, _)) = Set.singleton f
+    getCallsC (ConcurrentCall (f1, _, _) (f2, _, _)) = Set.fromList [f1, f2]
+    getCallsC (WithRes _ _ c) = getCallsC c
+    getCallsC (Block cs) = Set.unions $ map getCallsC cs
+
+
+-- todo: make this efficient.
+getCalledFunctions :: Context -> FunName -> Set FunName
+getCalledFunctions ctx fname =
+  go ctx (Set.singleton fname) fname 
+  where
+    go :: Context -> Set FunName -> FunName -> Set FunName
+    go ctx visited fname  =
+      let
+        calls = getCalls ctx fname
+        unvisitedCalls = calls `Set.difference` visited
+        visited' = visited \/ unvisitedCalls
+      in
+        if Set.null unvisitedCalls
+        then visited
+        else Set.unions $ Set.map (go ctx visited') unvisitedCalls
+
+
+
+
+genericCalled :: (Context -> Function -> Set String) -> Context -> FunName -> Set String
+genericCalled f ctx fname =
+  let functions = getCalledFunctions ctx fname in
+  Set.unions $ map (f ctx . getFunction ctx) (Set.toList functions)
+
+
+varCalled :: Context -> FunName -> Set VarName
+varCalled = genericCalled varF
+
+modCalled :: Context -> FunName -> Set VarName
+modCalled = genericCalled modF
+
+reqCalled :: Context -> FunName -> Set ResName
+reqCalled = genericCalled reqF
+
+
+
+
+
 varC :: Context -> Command -> Set VarName
 varC _ (Assign v expression) = Set.singleton v \/ fv expression
 varC _ (HeapLookup v expression _) = Set.singleton v \/ fv expression
@@ -107,9 +165,7 @@ varC _ (Dispose e) = fv e
 varC ctx (Block cs) = Set.unions $ map (varC ctx) cs
 varC ctx (IfThenElse b c1 c2) = fv b \/ varC ctx c1 \/ varC ctx c2
 varC ctx (While b i c) = fv b \/ fv i \/ varC ctx c
-varC ctx (Call (f, ps, es)) =
-  varF ctx fn \/ Set.fromList ps \/ fv es
-  where fn = getFunction ctx f
+varC _ (Call (_, ps, es)) = Set.fromList ps \/ fv es
 varC ctx (ConcurrentCall call1 call2) =
   varC ctx (Call call1) \/ varC ctx (Call call2)
 varC ctx (WithRes res b c) =
@@ -117,8 +173,8 @@ varC ctx (WithRes res b c) =
   where Resource _ _ inv = getResource ctx res
 
 varF :: Context -> Function -> Set VarName
-varF ctx (Function _ refs vals locals (p, c, q)) =
-  (varC ctx c \/ fv p \/ fv q) `Set.difference` Set.fromList (refs ++ vals ++ locals)
+varF ctx (Function fname refs vals locals (p, c, q)) =
+  (varCalled ctx fname \/ varC ctx c \/ fv p \/ fv q) `Set.difference` Set.fromList (refs ++ vals ++ locals)
 
 
 
@@ -133,9 +189,7 @@ modC _ (Dispose _) = Set.empty
 modC ctx (Block cs) = Set.unions $ map (modC ctx) cs
 modC ctx (IfThenElse _ c1 c2) = modC ctx c1 \/ modC ctx c2
 modC ctx (While _ _ c) = modC ctx c
-modC ctx (Call (f, ps, _)) =
-  modF ctx fn \/ Set.fromList ps
-  where fn = getFunction ctx f
+modC _ (Call (_, ps, _)) = Set.fromList ps
 modC ctx (ConcurrentCall call1 call2) =
   modC ctx (Call call1) \/ modC ctx (Call call2)
 modC ctx (WithRes r _ c) =
@@ -143,17 +197,16 @@ modC ctx (WithRes r _ c) =
 
 
 modF :: Context -> Function -> Set VarName
-modF ctx (Function _ refs vals locals (_, c, _)) =
-    modC ctx c `Set.difference` Set.fromList (refs ++ vals ++ locals)
+modF ctx (Function fname refs vals locals (_, c, _)) =
+    (modC ctx c \/ modCalled ctx fname) `Set.difference` Set.fromList (refs ++ vals ++ locals)
 
 
 reqC :: Context -> Command -> Set ResName
 reqC ctx (Block cs) = Set.unions $ map (reqC ctx) cs
 reqC ctx (IfThenElse b c1 c2) = reqC ctx c1 \/ reqC ctx c2 \/ er ctx Set.empty (fv b)
 reqC ctx (While b i c) = reqC ctx c \/ er ctx Set.empty (fv b \/ fv i)
-reqC ctx (Call (f, xs, es)) =
-  reqF ctx fn \/ er ctx (Set.fromList xs) (fv es)
-  where fn = getFunction ctx f
+reqC ctx (Call (_, xs, es)) =
+  er ctx (Set.fromList xs) (fv es)
 reqC ctx (ConcurrentCall call1 call2) =
   reqC ctx (Call call1) \/ reqC ctx (Call call2)
 reqC ctx (WithRes res b c) =
@@ -161,8 +214,8 @@ reqC ctx (WithRes res b c) =
 reqC ctx s = er ctx (modC ctx s) (varC ctx s)
 
 reqF :: Context -> Function -> Set ResName
-reqF ctx (Function _ refs vals locals (p, c, q)) =
-  reqC ctx c \/ er ctx Set.empty (fv p \/ fv q `Set.difference` Set.fromList (refs ++ vals ++ locals))
+reqF ctx (Function fname refs vals locals (p, c, q)) =
+  reqC ctx c \/ reqCalled ctx fname  \/ er ctx Set.empty (fv p \/ fv q `Set.difference` Set.fromList (refs ++ vals ++ locals))
 
 
 owned :: Context -> ResName -> Set VarName
