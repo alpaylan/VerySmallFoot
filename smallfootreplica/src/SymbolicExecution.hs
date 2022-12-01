a156a99e4331d97a77317a1fc29966aea15646f2
alpaylan <alpkeles99@gmail.com>
2022-12-01 15:35:34 -0500

    start adding symbolic execution primitives

---
+++ smallfootreplica/src/SymbolicExecution.hs
@@ -0,0 +1,42 @@
+module SymbolicExecution () where
+
+import Control.Monad.State
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Program
+import VCGen
+import VariableConditions
+
+type HeapFields = [(FieldName, Expression)]
+
+
+mutate :: HeapFields -> FieldName -> Expression -> HeapFields
+mutate p f e =
+    case Map.lookup f $ Map.fromList p of
+        Nothing -> (f, e) : p
+        Just _ -> map (\(f', e') -> if f == f' then (f, e) else (f', e')) p
+    
+
+lookup ::  Context -> HeapFields -> FieldName -> FreshVars (HeapFields, Expression)
+lookup ctx p f =
+    case Map.lookup f $ Map.fromList p of
+        Nothing -> do
+            v <- fresh
+            return ((f, Var v):p, Var v)
+        Just e -> return (p, e)
+
+
+
+
+    
+
+
+
+
+
+
+
